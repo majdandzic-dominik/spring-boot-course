035d62291dbc6f49e84510a9faf4054bdab9a556
unknown <dominik.majdandzic@gmail.com>
2024-03-19 11:43:33 +0100

    Added setter injection demo

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/dmajd/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,13 @@
+package com.dmajd.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach
+{
+    @Override
+    public String getDailyWorkou()
+    {
+        return "Practice fast bowling for 15 minutes!";
+    }
+}
---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/dmajd/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.dmajd.springcoredemo.rest;
+
+import com.dmajd.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController
+{
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach theCoach)
+    {
+        myCoach = theCoach;
+    }
+
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout()
+    {
+        return myCoach.getDailyWorkou();
+    }
+}
