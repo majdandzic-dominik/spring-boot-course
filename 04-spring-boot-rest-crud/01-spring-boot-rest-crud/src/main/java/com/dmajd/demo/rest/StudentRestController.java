9420caa2ef9d015b2ecfb7b9b8c142f76514387d
unknown <dominik.majdandzic@gmail.com>
2024-03-27 10:39:59 +0100

    Added getting student by id using path variables demo

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/dmajd/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/dmajd/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.dmajd.demo.rest;
 
 import com.dmajd.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,18 +14,31 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController
 {
-    // define endpoint for "/students" - return a list of students
+    private List<Student> students;
 
-    @GetMapping("/students")
-    public List<Student> getStudents()
+    // define @PostConstruct to load the student data only once
+    @PostConstruct
+    public void loadData()
     {
-        List<Student> students = new ArrayList<>();
+        students = new ArrayList<>();
 
         students.add(new Student("Adam", "Adamovich"));
         students.add(new Student("Sam", "Samovich"));
         students.add(new Student("Ken", "Kenovich"));
         students.add(new Student("Lucy", "Lucyovich"));
+    }
 
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents()
+    {
         return students;
+    }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("students/{studentId}")
+    public Student getStudentById(@PathVariable int studentId)
+    {
+        return students.get(studentId);
     }
 }
