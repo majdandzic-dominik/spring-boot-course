ddab8a4f2a5b102fac270e411cc77e7bb88e338b
unknown <dominik.majdandzic@gmail.com>
2024-04-03 12:07:27 +0200

    Added code for custom endpoint name and page size

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/dmajd/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/dmajd/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.dmajd.cruddemo.dao;
 
 import com.dmajd.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>
 {
 }
